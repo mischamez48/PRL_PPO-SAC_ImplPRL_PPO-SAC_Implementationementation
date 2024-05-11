a5d323246ca6e1405feadfd69a32c26262b9b92c
mischamez48 <mischa.mez@gmail.com>
2024-05-11 13:13:16 +0200

    last update for hyperparameter

--- ppo_algorithm.py
+++ ppo_algorithm.py
@@ -1,18 +1,18 @@
 import os
+import gym
 import numpy as np
+import matplotlib.pyplot as plt
 import torch as T
 import torch.nn as nn
 import torch.optim as optim
-from torch.distributions.categorical import Categorical
 import torch.nn.functional as F
+from torch.distributions.categorical import Categorical
 
 class PPOMemory:
     def __init__(self, batch_size):
         self.clear_memory()
         self.batch_size = batch_size
 
-    """List of int that correspond to the indices of our memories, and batch size chunks
-     of those memories"""
     def generate_batches(self):
         n_states = len(self.states)
         batch_start = np.arange(0, n_states, self.batch_size)
@@ -23,7 +23,7 @@ class PPOMemory:
         return (np.array(self.states), np.array(self.actions), np.array(self.probs),
                 np.array(self.vals), np.array(self.rewards), np.array(self.dones), batches)
 
-    def add_memory(self, state, action, probs, vals, reward, done): # store the memory for each trajectory
+    def add_memory(self, state, action, probs, vals, reward, done):
         self.states.append(state)
         self.actions.append(action)
         self.probs.append(probs)
@@ -31,7 +31,7 @@ class PPOMemory:
         self.rewards.append(reward)
         self.dones.append(done)
 
-    def clear_memory(self): # clear the memory after each trajectory
+    def clear_memory(self):
         self.states = []
         self.probs = []
         self.actions = []
@@ -40,51 +40,43 @@ class PPOMemory:
         self.vals = []
 
 class ActorNetwork(nn.Module):
-    def __init__(self, n_actions, input_dims, alpha,n_epochs, gamma, fc1_dims=256, fc2_dims=256):
+    def __init__(self, n_actions, input_dims, actor_alpha, n_epochs, gamma, fc1_dims=64, fc2_dims=64):
         super(ActorNetwork, self).__init__()
 
-        self.actor = nn.Sequential(
-                nn.Linear(*input_dims, fc1_dims),
-                nn.ReLU(),
-                nn.Linear(fc1_dims, fc2_dims),
-                nn.ReLU(),
-                nn.Linear(fc2_dims, n_actions),
-                nn.Softmax(dim=-1)
-        )
-
-        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
-        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=n_epochs,
-                                                   gamma=gamma)
+        self.fc1 = nn.Linear(*input_dims, fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+        self.fc3 = nn.Linear(fc2_dims, n_actions)
+        self.softmax = nn.Softmax(dim=-1)
+        self.optimizer = optim.Adam(self.parameters(), lr=actor_alpha)
+        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=n_epochs, gamma=gamma)
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
+
     def forward(self, state):
-        dist = self.actor(state)
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        dist = self.softmax(x)
         dist = Categorical(dist)
-        
         return dist
 
 class CriticNetwork(nn.Module):
-    def __init__(self, input_dims, alpha, n_epochs, gamma, fc1_dims=256, fc2_dims=256):
+    def __init__(self, input_dims, critic_alpha, n_epochs, gamma, fc1_dims=64, fc2_dims=64):
         super(CriticNetwork, self).__init__()
 
-        self.critic = nn.Sequential(
-                nn.Linear(*input_dims, fc1_dims),
-                nn.ReLU(),
-                nn.Linear(fc1_dims, fc2_dims),
-                nn.ReLU(),
-                nn.Linear(fc2_dims, 1)
-        )
-
-        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
-        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=n_epochs,
-                                                   gamma=gamma)
+        self.fc1 = nn.Linear(*input_dims, fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+        self.fc3 = nn.Linear(fc2_dims, 1)
+        self.optimizer = optim.Adam(self.parameters(), lr=critic_alpha)
+        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=n_epochs, gamma=gamma)
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
     def forward(self, state):
-        value = self.critic(state)
-
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        value = self.fc3(x)
         return value
     
 class AgentConfig:
@@ -94,16 +86,16 @@ class AgentConfig:
     eps_clip = 0.2
     batch_size = 5
     n_epochs = 4
-    gradient_clip = 0.5
     vf_coef = 1
     entropy_coef = 0.01
+    target_kl_div = 0.01
 
 class Agent(AgentConfig):
-    def __init__(self, n_actions, input_dims):
+    def __init__(self, n_actions, input_dims, actor_alpha, critic_alpha):
         super(Agent, self).__init__()
 
-        self.actor = ActorNetwork(n_actions, input_dims, self.alpha, self.n_epochs, self.gamma)
-        self.critic = CriticNetwork(input_dims, self.alpha, self.n_epochs, self.gamma)
+        self.actor = ActorNetwork(n_actions, input_dims, actor_alpha, self.n_epochs, self.gamma)
+        self.critic = CriticNetwork(input_dims, critic_alpha, self.n_epochs, self.gamma)
         self.memory = PPOMemory(self.batch_size)
 
     def store_transition(self, state, action, probs, vals, reward, done):
@@ -112,7 +104,6 @@ class Agent(AgentConfig):
     def select_action(self, observation):
         state = T.tensor([observation], dtype=T.float).to(self.actor.device)
         
-        # Get the action probabilities and critic value estimate for the given state
         action_distribution = self.actor(state)
         critic_value = self.critic(state)
         action = action_distribution.sample()
@@ -124,23 +115,24 @@ class Agent(AgentConfig):
         return action, log_prob, critic_value
 
 
-    def train(self):
+    def train(self,target_kl_div=0.01):
         for _ in range(self.n_epochs):
-            # Retrieve batches of data from memory
             states, actions, old_probs, values, rewards, dones, batches = self.memory.generate_batches()
-
-            # Calculate advantages
             advantages = self.calculate_advantages(rewards, values, dones)
-
             values = T.tensor(values).to(self.actor.device)
 
             for batch in batches:
-                total_losss = self.compute_losses(batch, states, actions, old_probs,values, advantages)
+                total_loss, states_batch, actions_batch = self.compute_losses(batch, states, actions, old_probs, values, advantages)
+                self.update_networks(total_loss)
 
-                # Update actor and critic networks
-                self.update_networks(total_losss)
+                # Compute KL divergence and break if it exceeds the target
+                old_log_probs = T.tensor(old_probs[batch]).to(self.actor.device)
+                dist = self.actor(states_batch)
+                new_log_probs = dist.log_prob(actions_batch)
+                kl_div = (old_log_probs - new_log_probs).mean()
+                if kl_div >= target_kl_div:
+                    break
 
-        # Clear memory after each epoch
         self.memory.clear_memory()
 
     def calculate_advantages(self, rewards, values, dones):
@@ -156,11 +148,10 @@ class Agent(AgentConfig):
 
         return T.tensor(advantage).to(self.actor.device)
 
-
     def compute_losses(self, batch, states, actions, old_probs, values, advantages):
         states_batch = T.tensor(states[batch], dtype=T.float).to(self.actor.device)
         old_probs_batch = T.tensor(old_probs[batch]).to(self.actor.device)
-        actions_batch = T.tensor(actions[batch]).to(self.actor.device)
+        actions_batch = T.tensor(actions[batch]).to(self.actor.device)  # Define actions_batch here
 
         dist = self.actor(states_batch)
         critic_value = self.critic(states_batch).squeeze()
@@ -176,20 +167,67 @@ class Agent(AgentConfig):
         returns = advantages[batch] + self.gamma * values[batch]
         vf_loss = 0.5*((returns - critic_value) ** 2).mean()
 
-        # Calculate entropy loss
         entropy = dist.entropy().mean()
 
         total_loss = clip_loss + self.vf_coef * vf_loss - self.entropy_coef * entropy
-        return total_loss
+        return total_loss, states_batch, actions_batch  # return states_batch and actions_batch for use in train function
+
+
 
     def update_networks(self, total_loss):
-        # Zero out gradients
         self.actor.optimizer.zero_grad()
         self.critic.optimizer.zero_grad()
         
-        # Backpropagation and optimization
         total_loss.backward()
-        nn.utils.clip_grad_norm_(self.actor.parameters(), self.gradient_clip)
-        nn.utils.clip_grad_norm_(self.critic.parameters(), self.gradient_clip)
         self.actor.optimizer.step()
         self.critic.optimizer.step()
+
+def plot_learning_curve(x, scores, figure_file):
+    running_avg = np.zeros(len(scores))
+    for i in range(len(running_avg)):
+        running_avg[i] = np.mean(scores[max(0, i-100):(i+1)])
+    plt.plot(x, running_avg)
+    plt.title('Running average of previous 100 scores')
+    plt.savefig(figure_file)
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v0')
+    N = 20
+    batch_size=8
+    agent = Agent(n_actions=env.action_space.n, input_dims=env.observation_space.shape, actor_alpha=3e-4, critic_alpha=1e-3)
+
+    n_games = 300
+
+    figure_file = 'plots/cartpoleV0.png'
+
+    best_score = env.reward_range[0]
+    score_history = []
+
+    learn_iters = 0
+    avg_score = 0
+    n_steps = 0
+
+    for i in range(n_games):
+        observation = env.reset()
+        done = False
+        score = 0
+        while not done:
+            action, prob, val = agent.select_action(observation)
+            observation_, reward, done, info = env.step(action)
+            n_steps += 1
+            score += reward
+            agent.store_transition(observation, action, prob, val, reward, done)
+            if n_steps % N == 0:
+                agent.train()
+                learn_iters += 1
+            observation = observation_
+        score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
+
+        if avg_score > best_score:
+            best_score = avg_score
+
+        print('episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score,
+                'time_steps', n_steps, 'learning_steps', learn_iters)
+    x = [i+1 for i in range(len(score_history))]
+    plot_learning_curve(x, score_history, figure_file)
